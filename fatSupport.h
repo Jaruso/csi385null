bbad4f701f413553d09de6b0b3c90234fb09c13c
Bar Baz <foo@bar.com>
2015-01-29 21:54:34 -0500

    Added fatSupport.h and fatSupport.c

---
+++ fatSupport.h
@@ -0,0 +1,8 @@
+//Team null
+//fatSupport.h from package.tgz
+
+int read_sector(int sector_number, char* buffer);
+int write_sector(int sector_number, char* buffer);
+
+unsigned int get_fat_entry(int fat_entry_number, char* fat);
+void set_fat_entry(int fat_entry_number, int value, char* fat);
