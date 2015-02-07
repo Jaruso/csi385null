bd5ee59636ac82bad042c183fa6f95fcc4fa613e
Brenden adamczak <cerebralasylum1@gmail.com>
2015-02-07 16:50:09 +0000

    you can type in pbs and it make file destination

--- shell.c
+++ shell.c
@@ -3,14 +3,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+/*don't know the best way to find the file location*/\
+/*
+Next going to start creating the shared memory and the mount function
+*/
 
-static struct {
+/*
+static struct { //maby this should be a array
     char* pfe;
     char* pbs;
+    char* mount;
 }Locations = {
     .pfe = "pfe/pfe",
-    .pbs = "pbs/pbs"
+    .pbs = "pbs/pbs",
+    .mount = "mount"
 };
+*/
+void findFileLocation(char * command){
+    char* both = malloc(strlen(command) *2 + 2); //going to have to unallocate this
+    strcpy(both, command);
+    strcat(both, "/");
+    strcat(both, command);
+    strcpy(command, both);
+    free(both);
+}
+
 
 int main(int argc, char** argv){
     puts("inside shell");
@@ -20,18 +37,38 @@ int main(int argc, char** argv){
     while(1){
         fgets(input,50,stdin);
         printf("your ouput %s", input );
+        
+        
+        
+        char* pch = strtok(input, " \n");
+        char* poop = "";
+        char* command = pch;
+        while(pch != NULL)
+        {
+            printf("%s\n",poop);
+            pch = strtok (NULL, " ");
+            asprintf(&poop, "%s %s", poop, pch); //concantinate strings
+        }
+        
+        
+        findFileLocation(command);
+        
+        printf("destination %s asdf \n\n", command);
+        
         pid = fork();
         if(pid > 0) //parent
         {
-            
+            //wait
         }
         else if (pid < 0) //error
         {
             exit(-1);
         }
         else{
-            execl(Locations.pbs,NULL);
+            execl(command,poop,NULL); //so it works when you put shell in pbs file but it doesn't work otherwise
         }
+        
+        
         
         break;
     }
