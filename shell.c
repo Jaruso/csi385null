094d5fad4b3f26100adefac584b7e3b9186b9f6b
Brenden adamczak <cerebralasylum1@gmail.com>
2015-02-08 16:31:29 +0000

    add shell.h and made it look pretty

--- shell.c
+++ shell.c
@@ -5,73 +5,67 @@
 #include <string.h>
 /*don't know the best way to find the file location*/\
 /*
-Next going to start creating the shared memory and the mount function
+Needs to be done
+shared memory
+directorys
 */
 
-/*
-static struct { //maby this should be a array
-    char* pfe;
-    char* pbs;
-    char* mount;
-}Locations = {
-    .pfe = "pfe/pfe",
-    .pbs = "pbs/pbs",
-    .mount = "mount"
-};
-*/
-void findFileLocation(char * command){
-    char* both = malloc(strlen(command) *2 + 2); //going to have to unallocate this
-    strcpy(both, command);
-    strcat(both, "/");
-    strcat(both, command);
-    strcpy(command, both);
-    free(both);
-}
+//on pg 4 and 5 they talk about how directorys are made
+
+
+#include "shell.h"
+
+typedef struct{
+    char * currentWorkingDirectory; //so this is going to be a pointer to the cwd inside the floppy drive
+    char * locationOfFloppyDrive;  //location of the actual floppy drive
+} sharedMemoryItems;
 
 
 int main(int argc, char** argv){
     puts("inside shell");
-    char* input=malloc((50)*sizeof(char));
+    char* input;
     printf("\n\tEnter a string : ");
+    char* command;
+    char* arguments;
+    char* location;
     pid_t pid;
     while(1){
+        input=malloc((50)*sizeof(char));
         fgets(input,50,stdin);
-        printf("your ouput %s", input );
+        printf("your ouput %s", input);
+        splitCommand(&command, &arguments, input);
+        location = findFileLocation(command);
         
-        
-        
-        char* pch = strtok(input, " \n");
-        char* arguments = "";
-        char* command = pch;
-        while(pch != NULL)
-        {
-            printf("%s\n",arguments);
-            pch = strtok (NULL, " ");
-            asprintf(&arguments, "%s %s", arguments, pch); //concantinate strings
-        }
-        
-        
-        findFileLocation(command);
-        
-        printf("destination %s asdf \n\n", command);
+        printf("first %s second %s", command, arguments);
         
         pid = fork();
         if(pid > 0) //parent
         {
-            //wait
+            wait(); //wait for all child process  //wait for specfic pid pg 154
+            puts("parents out\n");
         }
         else if (pid < 0) //error
         {
             exit(-1);
         }
         else{
-            execl(command,arguments,NULL); //so it works when you put shell in pbs file but it doesn't work otherwise
+            int ret;
+            if(arguments = ""){
+                ret = execl(location,NULL);
+            }
+            else{
+                ret = execl(location,arguments,NULL); //so it works when you put shell in pbs file but it doesn't work otherwise
+            }
+            if(ret == -1)//bad execl
+            {
+                perror("bad execl");
+            }
+            
         }
-        
-        
         
         break;
     }
     puts("exited shell");
+    free(input);
     return 0;
 }
